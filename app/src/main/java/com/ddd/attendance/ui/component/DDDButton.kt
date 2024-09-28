63c600b34c2b1dab102b6e8062faa0056ff479b6
imtreemiddle <treemiddle@musinsa.com>
2024-09-28 23:25:50 +0900

    button, text component

---
+++ app/src/main/java/com/ddd/attendance/ui/component/DDDButton.kt
@@ -0,0 +1,64 @@
+package com.ddd.attendance.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ddd.attendance.ui.theme.DDD_400
+import com.ddd.attendance.ui.theme.DDD_600
+import com.ddd.attendance.ui.theme.DDD_BLACK
+import com.ddd.attendance.ui.theme.DDD_WHITE
+
+@Composable
+fun DDDButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    isEnabled: Boolean = true,
+    onClick: () -> Unit = {}
+) {
+    Box(
+        modifier = modifier
+            .height(height = 56.dp)
+            .background(
+                color = if (isEnabled) {
+                    DDD_WHITE
+                } else {
+                    DDD_600
+                }
+            )
+            .clickable {
+                if (isEnabled) {
+                    onClick()
+                }
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        DDDText(
+            text = text,
+            color = if (isEnabled) {
+                DDD_BLACK
+            } else {
+                DDD_400
+            },
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Preview(name = "활성화 된 버튼", widthDp = 300)
+@Composable
+private fun P1() {
+    DDDButton(text = "다음")
+}
+
+@Preview(name = "비활성화 된 버튼", widthDp = 300)
+@Composable
+private fun P2() {
+    DDDButton(text = "다음", isEnabled = false)
+}
---
+++ app/src/main/java/com/ddd/attendance/ui/component/DDDText.kt
@@ -0,0 +1,25 @@
+package com.ddd.attendance.ui.component
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+
+@Composable
+fun DDDText(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    fontWeight: FontWeight? = null
+) {
+    Text(
+        text = text,
+        modifier = modifier,
+        color = color,
+        fontSize = fontSize,
+        fontWeight = fontWeight
+    )
+}
