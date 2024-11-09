1eeb0110197d07cc9a53e90669f92e2c07cb2c85
full_avocado <ses8323@gmail.com>
2024-11-09 16:16:48 +0900

    직무 선택 화면 구현

--- app/src/main/java/com/ddd/attendance/ui/screen/job/JobScreen.kt
+++ app/src/main/java/com/ddd/attendance/ui/screen/job/JobScreen.kt
@@ -3,10 +3,16 @@ package com.ddd.attendance.ui.screen.job
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -21,11 +27,13 @@ import androidx.navigation.NavController
 import com.ddd.attendance.ui.LoginProcessViewModel
 import com.ddd.attendance.ui.ScreenName
 import com.ddd.attendance.ui.component.DDDNextButton
+import com.ddd.attendance.ui.component.DDDSelector
 import com.ddd.attendance.ui.component.DDDText
 import com.ddd.attendance.ui.component.DDDTopBar
 import com.ddd.attendance.ui.component.TopBarType
 import com.ddd.attendance.ui.theme.DDD_800
 import com.ddd.attendance.ui.theme.DDD_BLACK
+import com.ddd.attendance.ui.theme.DDD_NEUTRAL_GRAY_20
 import com.ddd.attendance.ui.theme.DDD_WHITE
 
 @Composable
@@ -49,77 +57,69 @@ private fun Content(
     onClickBackButton: () -> Unit,
     onClickNext: () -> Unit
 ) {
+    val list = listOf(
+        "Product Manager",
+        "Product Designer",
+        "Android",
+        "iOS",
+        "Frontend",
+        "Backend",
+    )
     var selectedIndex by remember { mutableIntStateOf(value = -1) }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = DDD_BLACK)
-    ) {
+    Scaffold(
+        topBar = {
+            DDDTopBar(
+                type = TopBarType.LEFT_IMAGE,
+                onClickLeftImage = onClickBackButton
+            )
+            Spacer(modifier = Modifier.height(54.dp))
+        },
+        bottomBar = {
+            DDDNextButton(
+                text = "다음",
+                modifier = Modifier
+                    .fillMaxWidth(),
+                onClick = onClickNext,
+                isEnabled = selectedIndex != -1
+            )
+        }
+    ) { paddingValues ->
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .background(color = DDD_BLACK)
+                .padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            contentPadding = PaddingValues(horizontal = 24.dp)
         ) {
             item {
-                DDDTopBar(
-                    type = TopBarType.LEFT_IMAGE,
-                    onClickLeftImage = onClickBackButton
-                )
-            }
-            item {
                 DDDText(
-                    text = "직군을 선택해주세요",
-                    modifier = Modifier.padding(
-                        start = 32.dp,
-                        top = 54.dp,
-                        bottom = 48.dp
-                    ),
+                    text = "직무을 선택해주세요",
                     color = DDD_WHITE,
                     fontWeight = FontWeight.Bold,
                     fontSize = 24.sp
                 )
-            }
-            items(count = 6) {
+                Spacer(modifier = Modifier.height(8.dp))
                 DDDText(
-                    text = when (it) {
-                        0 -> "PM"
-                        1 -> "Designer"
-                        2 -> "Android"
-                        3 -> "iOS"
-                        4 -> "Front-end"
-                        5 -> "Back-end"
-                        else -> ""
-                    },
-                    color = if (selectedIndex == it) {
-                        DDD_WHITE
-                    } else {
-                        DDD_800
-                    },
-                    modifier = Modifier
-                        .padding(
-                            start = 32.dp,
-                            bottom = if (it != 6) { // 나중에 list lastindex로 교체
-                                24.dp
-                            } else {
-                                0.dp
-                            }
-                        )
-                        .clickable {
-                            selectedIndex = it
-                        },
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 32.sp
+                    text = "프로젝트 참여하시는 직무을 선택해 주세요.",
+                    color = DDD_NEUTRAL_GRAY_20,
+                    fontSize = 14.sp
+                )
+                Spacer(modifier = Modifier.height(40.dp))
+            }
+            itemsIndexed(list) { index, value ->
+                DDDSelector(
+                    text = value,
+                    selected = selectedIndex == index,
+                    onClick = { selectedIndex = index }
+                )
+                Spacer(
+                    modifier = Modifier.height(
+                        if (index != list.lastIndex) 8.dp else 0.dp
+                    )
                 )
             }
         }
-        DDDNextButton(
-            text = "다음",
-            modifier = Modifier
-                .align(alignment = Alignment.BottomCenter)
-                .fillMaxWidth(),
-            onClick = onClickNext,
-            isEnabled = selectedIndex != -1
-        )
     }
 }
